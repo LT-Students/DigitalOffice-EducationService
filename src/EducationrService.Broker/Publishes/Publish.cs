38961877d4e1907e803be50f27d0c2e4d03e7ab6
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-06-14 21:09:19 +0300

    create publish for removing images

---
+++ src/EducationrService.Broker/Publishes/Publish.cs
@@ -0,0 +1,26 @@
+﻿using LT.DigitalOffice.EducationService.Broker.Publishes.Interfaces;
+using LT.DigitalOffice.Models.Broker.Enums;
+using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Image;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.EducationService.Broker.Publishes
+{
+  public class Publish : IPublish
+  {
+    private readonly IBus _bus;
+
+    public Publish(IBus bus)
+    {
+      _bus = bus;
+    }
+    public async Task RemoveImagesAsync(List<Guid> imagesIds, ImageSource imageSource)
+    {
+      await _bus.Publish<IRemoveImagesPublish>(IRemoveImagesPublish.CreateObj(
+        imagesIds,
+        imageSource));
+    }
+  }
+}
