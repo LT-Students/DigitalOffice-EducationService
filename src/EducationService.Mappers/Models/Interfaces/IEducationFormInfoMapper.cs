bc1768fa0389004dcd2c5bebe3753442aee525b8
maybee16 <mayaochir@gmail.com>
2022-09-09 21:50:05 +0900

    Fixed mappers

--- src/EducationService.Mappers/Models/Interfaces/IEducationFormInfoMapper.cs
+++ src/EducationService.Mappers/Models/Interfaces/IEducationFormInfoMapper.cs
@@ -1,8 +1,10 @@
 ﻿using LT.DigitalOffice.EducationService.Models.Db;
 using LT.DigitalOffice.EducationService.Models.Dto.Models;
+using LT.DigitalOffice.Kernel.Attributes;
 
 namespace LT.DigitalOffice.EducationService.Mappers.Models.Interfaces;
 
+[AutoInject]
 public interface IEducationFormInfoMapper
 {
   EducationFormInfo Map(DbUserEducation dbUserEducation);
